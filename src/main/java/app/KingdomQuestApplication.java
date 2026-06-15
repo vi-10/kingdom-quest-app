79d3ce118018657965e4d9e3d004c91140d1f113
vi-10 <vaninailieva@gmail.com>
2026-06-16 02:59:41 +0300

    Initial commit

---
+++ src/main/java/app/KingdomQuestApplication.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingdomQuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KingdomQuestApplication.class, args);
+	}
+
+}

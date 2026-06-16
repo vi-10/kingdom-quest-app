9490ce662d80157cfb27bac1136edc3fdd409e70
vi-10 <vaninailieva@gmail.com>
2026-06-16 22:44:28 +0300

    feat: add HeroRepository

---
+++ src/main/java/app/repository/hero/HeroRepository.java
@@ -0,0 +1,11 @@
+package app.repository.hero;
+
+import app.model.entity.hero.Hero;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface HeroRepository extends JpaRepository<Hero, UUID> {
+}

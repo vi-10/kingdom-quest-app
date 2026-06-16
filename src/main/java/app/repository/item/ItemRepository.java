f8025843afb5546932c87d5f6a81e688c15271db
vi-10 <vaninailieva@gmail.com>
2026-06-16 22:44:41 +0300

    feat: add ItemRepository

---
+++ src/main/java/app/repository/item/ItemRepository.java
@@ -0,0 +1,11 @@
+package app.repository.item;
+
+import app.model.entity.item.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, UUID> {
+}

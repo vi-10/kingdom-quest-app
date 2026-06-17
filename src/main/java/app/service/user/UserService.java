fc1418e79100731aadf76220492063b87aa0bba5
vi-10 <vaninailieva@gmail.com>
2026-06-17 19:58:46 +0300

    feat: add UserService

---
+++ src/main/java/app/service/user/UserService.java
@@ -0,0 +1,17 @@
+package app.service.user;
+
+import app.repository.user.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class UserService {
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+}

6585d095110599052b8290de031d463c29b89d4c
vi-10 <vaninailieva@gmail.com>
2026-06-20 21:26:39 +0300

    feat: add toDto method in UserMapper

--- src/main/java/app/mapper/user/UserMapper.java
+++ src/main/java/app/mapper/user/UserMapper.java
@@ -1,9 +1,30 @@
 package app.mapper.user;
 
+import app.model.dto.user.UserDTO;
+import app.model.entity.user.User;
 import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
 
 @NoArgsConstructor
+@Component
 public class UserMapper {
+
+    public static UserDTO toUserDTO(User user){
+        if (user == null) {
+            return null;
+        }
+
+        return UserDTO.builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .email(user.getEmail())
+                .profilePicture(user.getProfilePicture())
+                .role(user.getRole())
+                .server(user.getServer())
+                .isActive(user.isActive())
+                .build();
+
+    }
 
 
 }

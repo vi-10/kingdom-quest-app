ee1e26a6c5eca525cc28a574ceaf28be7db8b262
vi-10 <vaninailieva@gmail.com>
2026-06-20 22:21:08 +0300

    feat: add POST mapping for user login

--- src/main/java/app/web/IndexController.java
+++ src/main/java/app/web/IndexController.java
@@ -2,12 +2,27 @@ package app.web;
 
 import app.model.dto.user.LoginDTO;
 import app.model.dto.user.RegisterDTO;
+import app.model.dto.user.UserDTO;
+import app.service.user.UserService;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
+    private UserService userService;
+
+    @Autowired
+    public IndexController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/")
     public String index(){
@@ -32,5 +47,19 @@ public class IndexController {
         return modelAndView;
     }
 
+    @PostMapping("/login")
+    public ModelAndView login(@Valid @ModelAttribute("loginData") LoginDTO loginData,
+                              BindingResult bindingResult,
+                              HttpSession session) {
 
+        if (bindingResult.hasErrors()) {
+            return new ModelAndView("login");
+        }
+
+        UserDTO user = userService.login(loginData);
+
+        session.setAttribute("userId", user.getId());
+
+        return new ModelAndView("redirect:/dashboard");
+    }
 }

0ff12b0f06bb020de894a948cfd0b5e35225ab43
vi-10 <vaninailieva@gmail.com>
2026-06-20 17:58:21 +0300

    feat: add passwordEncoder to UserService

--- src/main/java/app/service/user/UserService.java
+++ src/main/java/app/service/user/UserService.java
@@ -3,15 +3,20 @@ package app.service.user;
 import app.repository.user.UserRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @Transactional
 public class UserService {
     private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
+
+
 }

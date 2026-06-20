61fac160fa5cbd41efd8c94e4577ce9cf02383df
vi-10 <vaninailieva@gmail.com>
2026-06-20 21:34:13 +0300

    feat: implement login method in UserService

--- src/main/java/app/service/user/UserService.java
+++ src/main/java/app/service/user/UserService.java
@@ -1,10 +1,18 @@
 package app.service.user;
 
+import app.exception.InvalidCredentialsException;
+import app.exception.UserInactiveException;
+import app.mapper.user.UserMapper;
+import app.model.dto.user.LoginDTO;
+import app.model.dto.user.UserDTO;
+import app.model.entity.user.User;
 import app.repository.user.UserRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -17,6 +25,24 @@ public class UserService {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
+
+    public UserDTO login(LoginDTO loginData){
+        User user = userRepository.findByUsername(loginData.getUsername())
+                .orElseThrow(() ->
+                        new InvalidCredentialsException("Invalid username or password"));
+
+        if (!user.isActive()) {
+            throw new UserInactiveException("User account is inactive");
+        }
+
+        if (!passwordEncoder.matches(loginData.getPassword(), user.getPassword())) {
+            throw new InvalidCredentialsException("Invalid username or password");
+        }
+
+        return UserMapper.toUserDTO(user);
+    }
+
+
 
 
 }

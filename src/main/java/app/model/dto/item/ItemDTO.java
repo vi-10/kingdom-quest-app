ef61fb411ef024d359e498e30a75056cedf7e0fc
vi-10 <vaninailieva@gmail.com>
2026-06-20 19:21:40 +0300

    feat: add ItemDTO

---
+++ src/main/java/app/model/dto/item/ItemDTO.java
@@ -0,0 +1,22 @@
+package app.model.dto.item;
+
+import app.model.dto.hero.HeroDTO;
+import app.model.entity.hero.Hero;
+import app.model.entity.hero.HeroClass;
+import app.model.entity.item.ItemRarity;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+@Builder
+public class ItemDTO {
+
+    private UUID id;
+    private String name;
+    private HeroClass heroClass;
+    private int requiredGold;
+    private ItemRarity rarity;
+
+}

94359ff42bfffe3b57863749f61f4b847bdcf298
vi-10 <vaninailieva@gmail.com>
2026-06-21 16:39:40 +0300

    feat: add ItemMapper

---
+++ src/main/java/app/mapper/item/ItemMapper.java
@@ -0,0 +1,21 @@
+package app.mapper.item;
+
+import app.model.dto.item.ItemDTO;
+import app.model.entity.item.Item;
+
+
+public class ItemMapper {
+    public static ItemDTO toItemDTO(Item item){
+        if (item == null) {
+            return null;
+        }
+
+        return ItemDTO.builder()
+                .id(item.getId())
+                .name(item.getName())
+                .heroClass(item.getHeroClass())
+                .requiredGold(item.getRequiredGold())
+                .rarity(item.getRarity())
+                .build();
+    }
+}

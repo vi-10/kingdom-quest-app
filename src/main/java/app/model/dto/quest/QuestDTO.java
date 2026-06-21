33b08eec92df096fd5fb2d567c34e0d7516a6846
vi-10 <vaninailieva@gmail.com>
2026-06-21 15:00:31 +0300

    feat: add QuestDTO

---
+++ src/main/java/app/model/dto/quest/QuestDTO.java
@@ -0,0 +1,21 @@
+package app.model.dto.quest;
+
+import app.model.entity.quest.QuestType;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+@Builder
+public class QuestDTO {
+
+    private UUID id;
+    private String title;
+    private String description;
+    private QuestType questType;
+    private int requiredLevel;
+    private int rewardXp;
+    private int rewardGold;
+}

78de81ea74e37bb4f7343f3c7398a033ac702b29
vi-10 <vaninailieva@gmail.com>
2026-06-21 16:40:07 +0300

    feat: add getAllItems method in ItemService

--- src/main/java/app/service/item/ItemService.java
+++ src/main/java/app/service/item/ItemService.java
@@ -1,9 +1,15 @@
 package app.service.item;
 
+import app.mapper.item.ItemMapper;
+import app.mapper.quest.QuestMapper;
+import app.model.dto.item.ItemDTO;
+import app.model.dto.quest.QuestDTO;
 import app.repository.item.ItemRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Transactional
@@ -13,5 +19,9 @@ public class ItemService {
     @Autowired
     public ItemService(ItemRepository itemRepository) {
         this.itemRepository = itemRepository;
+    }
+
+    public List<ItemDTO> getAllItems() {
+        return itemRepository.findAll().stream().map(ItemMapper::toItemDTO).toList();
     }
 }

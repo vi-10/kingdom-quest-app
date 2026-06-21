cac884f407683e4c67f2f73f276d3af6edd1cb1d
vi-10 <vaninailieva@gmail.com>
2026-06-21 13:33:07 +0300

    feat: add getByUserId method in HeroService

--- src/main/java/app/service/hero/HeroService.java
+++ src/main/java/app/service/hero/HeroService.java
@@ -1,9 +1,15 @@
 package app.service.hero;
 
+import app.exception.UserNotFoundException;
+import app.mapper.hero.HeroMapper;
+import app.model.dto.hero.HeroDTO;
+import app.model.entity.hero.Hero;
 import app.repository.hero.HeroRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @Transactional
@@ -13,5 +19,10 @@ public class HeroService {
     @Autowired
     public HeroService(HeroRepository heroRepository) {
         this.heroRepository = heroRepository;
+    }
+
+    public HeroDTO getByUserId(UUID userId) {
+        Hero hero = heroRepository.findByUserId(userId).orElseThrow(() -> new UserNotFoundException(String.format("User with %s id doesn't exist.", userId)));
+        return HeroMapper.toHeroDTO(hero);
     }
 }

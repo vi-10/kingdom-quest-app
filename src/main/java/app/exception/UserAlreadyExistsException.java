a844cbb5a94e6f82a48039f2c686360f7f677061
vi-10 <vaninailieva@gmail.com>
2026-06-21 12:02:34 +0300

    feat: add UserAlreadyExistsException

---
+++ src/main/java/app/exception/UserAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package app.exception;
+
+public class UserAlreadyExistsException extends RuntimeException {
+
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
+}

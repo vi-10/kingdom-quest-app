0f720e353b7e1ac2d97c3e456ebf7668478cc96e
vi-10 <vaninailieva@gmail.com>
2026-06-21 12:49:17 +0300

    feat: add findByUserId method in HeroRepository

--- src/main/java/app/repository/hero/HeroRepository.java
+++ src/main/java/app/repository/hero/HeroRepository.java
@@ -4,8 +4,11 @@ import app.model.entity.hero.Hero;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface HeroRepository extends JpaRepository<Hero, UUID> {
+
+    Optional<Hero> findByUserId(UUID userId);
 }

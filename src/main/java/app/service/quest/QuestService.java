ff9d8ead0da2fcf5e6cc5788d48f451dc7506c19
vi-10 <vaninailieva@gmail.com>
2026-06-21 15:06:55 +0300

    feat: add getAllQuests in QuestService

--- src/main/java/app/service/quest/QuestService.java
+++ src/main/java/app/service/quest/QuestService.java
@@ -1,9 +1,13 @@
 package app.service.quest;
 
+import app.mapper.quest.QuestMapper;
+import app.model.dto.quest.QuestDTO;
 import app.repository.quest.QuestRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Transactional
@@ -13,5 +17,9 @@ public class QuestService {
     @Autowired
     public QuestService(QuestRepository questRepository) {
         this.questRepository = questRepository;
+    }
+
+    public List<QuestDTO> getAllQuests() {
+        return questRepository.findAll().stream().map(QuestMapper::toQuestDTO).toList();
     }
 }

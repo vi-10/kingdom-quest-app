a7bbda7540bb461663a1869c718556c3df00533e
vi-10 <vaninailieva@gmail.com>
2026-06-21 17:38:28 +0300

    feat: add forgeItem method in ItemService

--- src/main/java/app/service/item/ItemService.java
+++ src/main/java/app/service/item/ItemService.java
@@ -1,27 +1,68 @@
 package app.service.item;
 
+import app.exception.ItemNotFoundException;
+import app.exception.QuestNotFoundException;
+import app.exception.UserNotFoundException;
 import app.mapper.item.ItemMapper;
 import app.mapper.quest.QuestMapper;
+import app.model.dto.item.ForgeResultDTO;
 import app.model.dto.item.ItemDTO;
 import app.model.dto.quest.QuestDTO;
+import app.model.entity.hero.Hero;
+import app.model.entity.heroitem.HeroItem;
+import app.model.entity.item.Item;
+import app.model.entity.quest.Quest;
+import app.repository.hero.HeroRepository;
+import app.repository.heroitem.HeroItemRepository;
 import app.repository.item.ItemRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @Transactional
 public class ItemService {
     private ItemRepository itemRepository;
+    private HeroRepository heroRepository;
+    private HeroItemRepository heroItemRepository;
 
     @Autowired
-    public ItemService(ItemRepository itemRepository) {
+    public ItemService(ItemRepository itemRepository, HeroRepository heroRepository, HeroItemRepository heroItemRepository) {
         this.itemRepository = itemRepository;
+        this.heroRepository = heroRepository;
+        this.heroItemRepository = heroItemRepository;
     }
 
     public List<ItemDTO> getAllItems() {
         return itemRepository.findAll().stream().map(ItemMapper::toItemDTO).toList();
+    }
+
+    public ForgeResultDTO forgeItem(UUID itemId, UUID userId) {
+
+        Hero hero = heroRepository.findByUserId(userId)
+                .orElseThrow(() -> new UserNotFoundException("User not found"));
+
+        Item item = itemRepository.findById(itemId)
+                .orElseThrow(() -> new ItemNotFoundException("Item not found"));
+
+        if (hero.getHeroClass() != item.getHeroClass()) {
+            return ForgeResultDTO.builder().message("Incorrect hero class.").build();
+        }
+
+        if (hero.getGold() < item.getRequiredGold()) {
+            return ForgeResultDTO.builder().message("Not enough gold.").build();
+        }
+
+        hero.setGold(hero.getGold() - item.getRequiredGold());
+
+        HeroItem heroItem = HeroItem.builder().hero(hero).item(item).build();
+
+        heroItemRepository.save(heroItem);
+        heroRepository.save(hero);
+
+        return null;
     }
 }

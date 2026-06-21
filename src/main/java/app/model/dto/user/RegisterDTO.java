a36dd91eaa27c7f52fbac456381fd52d7bc19cd8
vi-10 <vaninailieva@gmail.com>
2026-06-21 11:40:32 +0300

    fix: add email in RegisterDTO and fix validation constrains

--- src/main/java/app/model/dto/user/RegisterDTO.java
+++ src/main/java/app/model/dto/user/RegisterDTO.java
@@ -2,6 +2,8 @@ package app.model.dto.user;
 
 import app.model.entity.hero.HeroClass;
 import app.model.entity.user.Server;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
@@ -10,15 +12,24 @@ import lombok.Data;
 @Data
 @Builder
 public class RegisterDTO {
+    @NotBlank(message = "Username must not be blank.")
     @Size(min = 5, message = "Username must be at least 5 characters")
     private String username;
+
+    @NotBlank(message = "Password must not be blank.")
     @Size(min = 5, message = "Password must be at least 5 characters")
     private String password;
-    @Size(min = 5, message = "Roleplay name must be at least 5 characters")
+
+    @Email(message = "Please enter a valid email.")
+    @NotBlank(message = "Email must not be blank.")
+    private String email;
+
+    @NotBlank(message = "Roleplay name must not be blank.")
     private String roleplayName;
+
     @NotNull(message = "Hero class must not be null.")
     private HeroClass heroClass;
+
     @NotNull(message = "Server must not be null.")
     private Server server;
-
 }

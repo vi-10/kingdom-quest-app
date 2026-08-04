d2d2fa7ff46aeaa283690d44814c2ecd11f16794
vi-10 <vaninailieva@gmail.com>
2026-08-05 01:45:20 +0300

    refactor: remove unused imports from WebMvcConfiguration

--- src/main/java/app/config/WebMvcConfiguration.java
+++ src/main/java/app/config/WebMvcConfiguration.java
@@ -1,14 +1,11 @@
 package app.config;
 
-import app.security.SessionInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration

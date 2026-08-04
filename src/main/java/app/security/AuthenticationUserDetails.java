f7843c201ac945f48a84f7667d422d31b3f82e6a
vi-10 <vaninailieva@gmail.com>
2026-08-04 23:50:10 +0300

    feat: add AuthenticationUserDetails implementing UserDetails

---
+++ src/main/java/app/security/AuthenticationUserDetails.java
@@ -0,0 +1,60 @@
+package app.security;
+
+import app.model.entity.user.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class AuthenticationUserDetails implements UserDetails {
+    private UUID id;
+    private String username;
+    private String password;
+    private Role role;
+    private boolean isActive;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isActive;
+    }
+}

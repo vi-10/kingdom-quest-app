ada23e7b6fda2ed5130182b436470a05ad1fcf12
vi-10 <vaninailieva@gmail.com>
2026-08-06 18:59:25 +0300

    feat: add EventService

---
+++ src/main/java/app/service/event/EventService.java
@@ -0,0 +1,17 @@
+package app.service.event;
+
+import app.model.dto.event.ActiveEventResponse;
+import app.service.event.client.EventClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EventService {
+    private final EventClient eventClient;
+
+    public ActiveEventResponse getActiveEvent(){
+        return eventClient.getActiveEvent().getBody();
+    }
+}

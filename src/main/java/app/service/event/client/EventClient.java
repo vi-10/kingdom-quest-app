415bed1fdc04ebaa4099a95285819b4c19ba126e
vi-10 <vaninailieva@gmail.com>
2026-08-07 20:16:27 +0300

    feat: add createEvent in EventClient

--- src/main/java/app/service/event/client/EventClient.java
+++ src/main/java/app/service/event/client/EventClient.java
@@ -1,11 +1,11 @@
 package app.service.event.client;
 
 import app.model.dto.event.ActiveEventResponse;
+import app.model.dto.event.CreateEventRequest;
+import jakarta.validation.Valid;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,5 +14,10 @@ public interface EventClient {
 
     @GetMapping
     ResponseEntity<ActiveEventResponse> getActiveEvent();
+
+    @PostMapping
+    ResponseEntity<Void> createEvent(
+            @RequestBody CreateEventRequest request
+    );
 
 }

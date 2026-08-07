7cd6f29222fa81025b93cfc4bfe58502b7d30e4f
vi-10 <vaninailieva@gmail.com>
2026-08-07 20:20:54 +0300

    feat: add getCreateEventPage and createEvent in EventController

--- src/main/java/app/web/event/EventController.java
+++ src/main/java/app/web/event/EventController.java
@@ -1,9 +1,15 @@
 package app.web.event;
 
 import app.model.dto.event.ActiveEventResponse;
+import app.model.dto.event.CreateEventRequest;
+import app.model.dto.quest.CreateQuestDTO;
 import app.service.event.EventService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -25,5 +31,31 @@ public class EventController {
         return modelAndView;
 
      }
+
+    @GetMapping("/admin/events/create")
+    public ModelAndView getCreateEventPage() {
+
+        ModelAndView modelAndView = new ModelAndView("create-event");
+        CreateEventRequest request = CreateEventRequest.builder().build();
+        modelAndView.addObject("eventData", request);
+
+        return modelAndView;
+    }
+
+    @PostMapping("/admin/events/create")
+    public String createEvent(
+            @Valid @ModelAttribute("eventData") CreateEventRequest request,
+            BindingResult bindingResult) {
+
+        if(bindingResult.hasErrors()) {
+            return "create-event";
+        }
+
+        eventService.createEvent(request);
+
+        return "redirect:/admin/events";
+    }
+
+
 
 }

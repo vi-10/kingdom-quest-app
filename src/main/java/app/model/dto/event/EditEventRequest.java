8caf2c7c1cdce716808a98cd5064b3f276915e20
vi-10 <vaninailieva@gmail.com>
2026-08-07 23:04:09 +0300

    feat: add EditEventRequest

---
+++ src/main/java/app/model/dto/event/EditEventRequest.java
@@ -0,0 +1,45 @@
+package app.model.dto.event;
+
+import app.model.entity.quest.QuestType;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EditEventRequest {
+    @NotNull
+    private UUID id;
+
+    @NotBlank(message = "Event title must not be blank.")
+    private String title;
+
+    @NotBlank(message = "Description must not be blank.")
+    private String description;
+
+    @NotNull(message = "Quest type must not be null.")
+    private QuestType affectedQuestType;
+
+    @NotNull(message = "Bonus XP must not be null.")
+    @Min(value = 0, message = "Bonus XP cannot be negative.")
+    private Integer bonusXp;
+
+    @NotNull(message = "Bonus gold must not be null.")
+    @Min(value = 0, message = "Bonus gold cannot be negative.")
+    private Integer bonusGold;
+
+    @NotNull(message = "Start time must not be null.")
+    private LocalDateTime start;
+
+    @NotNull(message = "End time must not be null.")
+    private LocalDateTime end;
+}

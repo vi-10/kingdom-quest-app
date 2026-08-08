85822a7a539f903bd2151616b4e5f54f027cb096
vi-10 <vaninailieva@gmail.com>
2026-08-09 02:02:45 +0300

    refactor: delete InvalidCredentialsException

--- src/main/java/app/exception/InvalidCredentialsException.java
+++
@@ -1,7 +0,0 @@
-package app.exception;
-
-public class InvalidCredentialsException extends RuntimeException{
-    public InvalidCredentialsException(String message) {
-        super(message);
-    }
-}

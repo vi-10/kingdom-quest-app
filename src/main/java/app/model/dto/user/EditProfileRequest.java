5ae92ae6bb7e9dcfe66fadea8bf5359eef94e600
vi-10 <vaninailieva@gmail.com>
2026-08-08 18:51:39 +0300

    feat: add EditProfileRequest

---
+++ src/main/java/app/model/dto/user/EditProfileRequest.java
@@ -0,0 +1,25 @@
+package app.model.dto.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+public class EditProfileRequest {
+
+    @NotBlank(message = "Username must not be blank.")
+    private String username;
+
+    @NotBlank(message = "Email must not be blank.")
+    @Email(message = "Please enter a valid email address.")
+    private String email;
+
+    private String profilePicture;
+
+    @NotBlank(message = "Roleplay name must not be blank.")
+    private String roleplayName;
+}

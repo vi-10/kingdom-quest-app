e1c190a273f1836b1c3c11863b88c2498ce48106
vi-10 <vaninailieva@gmail.com>
2026-08-09 20:25:47 +0300

    feat: add @EnableWebSecurity and @EnableMethodSecurity to WebMvcConfiguration

--- src/main/java/app/config/WebMvcConfiguration.java
+++ src/main/java/app/config/WebMvcConfiguration.java
@@ -4,12 +4,17 @@ import app.security.CustomAuthenticationFailureHandler;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
     private CustomAuthenticationFailureHandler customAuthenticationFailureHandler;

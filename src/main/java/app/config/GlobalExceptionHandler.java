7379b942deaa5be5c19570f4389ff0ab028b796b
vi-10 <vaninailieva@gmail.com>
2026-08-09 03:45:18 +0300

    feat: add handleFeignException in GlobalExceptionHandler

--- src/main/java/app/config/GlobalExceptionHandler.java
+++ src/main/java/app/config/GlobalExceptionHandler.java
@@ -1,15 +1,23 @@
 package app.config;
 
 import app.exception.ApplicationException;
+import app.model.dto.event.ErrorResponseDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import feign.FeignException;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.IOException;
+
 @Slf4j
 @ControllerAdvice
+@RequiredArgsConstructor
 public class GlobalExceptionHandler {
+
+    private final ObjectMapper objectMapper;
 
     @ExceptionHandler(ApplicationException.class)
     public ModelAndView handleApplicationException(ApplicationException e) {
@@ -19,6 +27,20 @@ public class GlobalExceptionHandler {
         modelAndView.addObject("errorCode", e.getErrorCode());
         modelAndView.addObject("errorTitle", e.getErrorTitle());
         return modelAndView; }
+
+    @ExceptionHandler(FeignException.FeignClientException.class)
+    public ModelAndView handleFeignException(FeignException ex) throws IOException {
+        log.error("FeignException occurred: {}", ex.getMessage(), ex);
+
+        ErrorResponseDTO errorResponse = objectMapper.readValue(ex.contentUTF8(), ErrorResponseDTO.class);
+
+        ModelAndView modelAndView = new ModelAndView("error");
+        modelAndView.addObject( "errorMessage", errorResponse.getMessage());
+        modelAndView.addObject( "errorCode", errorResponse.getErrorCode());
+        modelAndView.addObject( "errorTitle", errorResponse.getErrorTitle());
+
+        return modelAndView;
+    }
 
     @ExceptionHandler(Exception.class)
     public ModelAndView handleException(Exception e) {

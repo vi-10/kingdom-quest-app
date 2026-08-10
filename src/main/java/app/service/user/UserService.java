e6d240b43dde3eef57b58ed35d117e0b7b96a161
vi-10 <vaninailieva@gmail.com>
2026-08-10 23:10:14 +0300

    feat: add logs in UserService

--- src/main/java/app/service/user/UserService.java
+++ src/main/java/app/service/user/UserService.java
@@ -14,6 +14,7 @@ import app.repository.hero.HeroRepository;
 import app.repository.user.UserRepository;
 import app.security.AuthenticationUserDetails;
 import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -25,6 +26,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Slf4j
 @Service
 @Transactional
 public class UserService implements UserDetailsService {
@@ -40,7 +42,6 @@ public class UserService implements UserDetailsService {
     }
 
     private String getDefaultProfilePicture(HeroClass heroClass) {
-
         return switch (heroClass) {
             case WARRIOR -> "/images/warrior.jpeg";
             case MAGE -> "/images/mage.png";
@@ -50,6 +51,7 @@ public class UserService implements UserDetailsService {
     }
 
     public UserDTO register(RegisterDTO registerData) {
+        log.info("Registering new user with username '{}'", registerData.getUsername());
 
         if (userRepository.existsByUsername(registerData.getUsername())) {
             throw new UserAlreadyExistsException(registerData.getUsername());
@@ -79,37 +81,67 @@ public class UserService implements UserDetailsService {
         userRepository.save(user);
         heroRepository.save(hero);
 
+        log.info("User '{}' registered successfully with ID {}", user.getUsername(), user.getId());
+
         return UserMapper.toUserDTO(user);
     }
 
 
     public UserDTO getById(UUID id) {
+        log.debug("Fetching user with ID {}", id);
+
         User user = userRepository.findById(id).orElseThrow(UserNotFoundException::new);
+
         return UserMapper.toUserDTO(user);
     }
 
     public List<UserDTO> getAllUsers() {
-        return userRepository.findAll().stream().map(UserMapper::toUserDTO).toList();
+        log.debug("Fetching all users");
+
+        List<UserDTO> users =  userRepository.findAll().stream().map(UserMapper::toUserDTO).toList();
+
+        log.debug("Fetched {} users", users.size());
+
+        return users;
     }
 
     public void switchRole(UUID id) {
+        log.info("Switching role for user with ID {}", id);
+
         User user = userRepository.findById(id).orElseThrow(UserNotFoundException::new);
+
+        Role oldRole = user.getRole();
+
         if (user.getRole() == Role.USER) {
             user.setRole(Role.ADMIN);
         } else {
             user.setRole(Role.USER);
         }
         userRepository.save(user);
+
+        log.info("User with ID {} role changed from {} to {}", id, oldRole, user.getRole());
     }
 
     public void switchStatus(UUID id) {
+        log.info("Switching active status for user with ID {}", id);
+
         User user = userRepository.findById(id).orElseThrow(UserNotFoundException::new);
+
+        boolean oldStatus = user.isActive();
+
         user.setActive(!user.isActive());
         userRepository.save(user);
+
+        if(oldStatus){
+            log.info("User with ID {} status changed to inactive.", id);
+        } else {
+            log.info("User with ID {} status changed to active.", id);
+        }
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.debug("Loading authentication details for username '{}'", username);
 
         User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException(username));
 
@@ -123,6 +155,7 @@ public class UserService implements UserDetailsService {
     }
 
     public void editProfile(UUID userId, EditProfileRequest request) {
+        log.info("Editing profile for user with ID {}", userId);
 
         User user = userRepository.findById(userId)
                 .orElseThrow(UserNotFoundException::new);
@@ -147,5 +180,7 @@ public class UserService implements UserDetailsService {
         hero.setRoleplayName(request.getRoleplayName());
 
         userRepository.save(user);
+
+        log.info("Profile for user with ID {} updated successfully", userId);
     }
 }

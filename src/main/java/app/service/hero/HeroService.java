d1ca4c840944b7919704ca181fdbe2910a3d16b6
vi-10 <vaninailieva@gmail.com>
2026-08-10 23:47:36 +0300

    feat: add logs in HeroService

--- src/main/java/app/service/hero/HeroService.java
+++ src/main/java/app/service/hero/HeroService.java
@@ -6,11 +6,13 @@ import app.model.dto.hero.HeroDTO;
 import app.model.entity.hero.Hero;
 import app.repository.hero.HeroRepository;
 import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 @Transactional
 public class HeroService {
@@ -22,7 +24,12 @@ public class HeroService {
     }
 
     public HeroDTO getByUserId(UUID userId) {
+        log.debug("Fetching hero for user {}", userId);
+
         Hero hero = heroRepository.findByUserId(userId).orElseThrow(HeroNotFoundException::new);
+
+        log.debug("Hero found for user {}", userId);
+
         return HeroMapper.toHeroDTO(hero);
     }
 }
